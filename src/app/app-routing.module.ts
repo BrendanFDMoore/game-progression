a90ee675be13b763c5a6d188bdf6e8664d3021a2
Brendan Moore <brendan.moore@rangle.io>
2019-02-07 15:42:03 -0500

    Styling active menu item Redirecting junk routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', loadChildren: './features/dashboard/dashboard.module#DashboardModule' },
   { path: 'games', loadChildren: './features/games/games.module#GamesModule' },
+  { path: '**', redirectTo: '/'},
 ];
 
 @NgModule({
