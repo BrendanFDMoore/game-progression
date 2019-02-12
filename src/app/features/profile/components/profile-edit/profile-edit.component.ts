61d9a045ccccae0b2cef513cbedec8ba15b4503e
Brendan Moore <brendan.moore@rangle.io>
2019-02-12 14:37:54 -0500

    Switch to FormGroup

--- src/app/features/profile/components/profile-edit/profile-edit.component.ts
+++ src/app/features/profile/components/profile-edit/profile-edit.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { ProfileService } from '../../services/profile.service';
+
+// interface MyFormModel {
+//   firstName: string;
+//   lastName: string;
+//   averageHours: string;
+//   avatarUrl: string;
+// }
 
 @Component({
   selector: 'gp-profile-edit',
@@ -11,11 +18,12 @@ import { ProfileService } from '../../services/profile.service';
 export class ProfileEditComponent implements OnInit {
   public profile: any;
   public ready = false;
-
-  firstNameControl = new FormControl('');
-  lastNameControl = new FormControl('');
-  hoursControl = new FormControl('');
-  avatarUrlControl = new FormControl('');
+  profileForm: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    averageHours: new FormControl(''),
+    avatarUrl: new FormControl(''),
+  });
 
   constructor (
     private profileService: ProfileService,
@@ -27,10 +35,16 @@ export class ProfileEditComponent implements OnInit {
   }
 
   fillFormFromProfile() {
-    this.firstNameControl.setValue(this.profile.firstName);
-    this.lastNameControl.setValue(this.profile.lastName);
-    this.hoursControl.setValue(this.profile.averageNumberOfHoursPerDay);
-    this.avatarUrlControl.setValue(this.profile.image);
+    // this.profileForm.firstName.setValue(this.profile.firstName);
+    // this.profileForm.lastName.setValue(this.profile.lastName);
+    // this.profileForm.averageHours.setValue(this.profile.averageNumberOfHoursPerDay);
+    // this.profileForm.avatarUrl.setValue(this.profile.image);
+    this.profileForm.patchValue({
+      firstName: this.profile.firstName,
+      lastName: this.profile.lastName,
+      averageHours: this.profile.averageHours,
+      avatarUrl: this.profile.image,
+    });
   }
 
   ngOnInit() {
