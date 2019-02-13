56ad11c4486f09d63bf3e53402d45bf651ad92ca
Brendan Moore <brendan.moore@rangle.io>
2019-02-13 15:08:18 -0500

    👮‍♂️Add form validation

--- src/app/features/profile/components/profile-edit/profile-edit.component.ts
+++ src/app/features/profile/components/profile-edit/profile-edit.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 import { ProfileService } from '../../services/profile.service';
 
-// interface MyFormModel {
+// interface ProfileForm {
 //   firstName: string;
 //   lastName: string;
 //   averageHours: string;
 //   avatarUrl: string;
 // }
+
+const isAlpha = s => {
+  const result = /^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/.test(s);
+  console.log({isAlpha: result});
+  return result;
+};
+
+export function regexValidator(regex: RegExp): ValidatorFn {
+  console.log('regexValidator outer');
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    console.log('regexValidator inner', control.value);
+    return regex.test(control.value) ? null : {'regexValidator': {value: control.value}};
+  };
+}
+
+export function alphaValidator(control: AbstractControl): {[key: string]: any} | null {
+  console.log('alphaValidator inner', control.value);
+  // const alpha = isAlpha(control.value);
+  return isAlpha(control.value) ? null : {'alphaValidator': {value: control.value}};
+}
 
 @Component({
   selector: 'gp-profile-edit',
@@ -19,10 +39,10 @@ export class ProfileEditComponent implements OnInit {
   public profile: any;
   public ready = false;
   profileForm: FormGroup = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    averageHours: new FormControl(''),
-    avatarUrl: new FormControl(''),
+    firstName: new FormControl('', [Validators.required, regexValidator(/^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/)]),
+    lastName: new FormControl('', [Validators.required, alphaValidator]),
+    averageHours: new FormControl('', [Validators.required, regexValidator(/^[1-9][0-9]*$/)]),
+    avatarUrl: new FormControl('', Validators.required),
   });
 
   constructor (
@@ -30,15 +50,13 @@ export class ProfileEditComponent implements OnInit {
   ) { }
 
   cancel() {
-    this.fillFormFromProfile();
+    // confirmation dialog
+    console.log(this.profileForm.controls.firstName);
+    // this.fillFormFromProfile();
     // nav to profile?
   }
 
   fillFormFromProfile() {
-    // this.profileForm.firstName.setValue(this.profile.firstName);
-    // this.profileForm.lastName.setValue(this.profile.lastName);
-    // this.profileForm.averageHours.setValue(this.profile.averageNumberOfHoursPerDay);
-    // this.profileForm.avatarUrl.setValue(this.profile.image);
     this.profileForm.patchValue({
       firstName: this.profile.firstName,
       lastName: this.profile.lastName,
