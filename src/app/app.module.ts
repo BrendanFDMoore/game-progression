7870d27f927a6b18670260a81019c0c58d17b070
Brendan Moore <brendan.moore@rangle.io>
2019-02-15 12:57:16 -0500

    Add Ngrx version of profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'; // Angular CLI environemnt
+
+import { EffectsModule } from '@ngrx/effects';
+
 import { AppRoutingModule } from './app-routing.module';
 import { ProfileService } from './root/services/profile.service';
+
+import { profileReducer, initialProfileState } from './features/profile/services/profile.service';
+import { ProfileEffects } from './features/profile/services/profile.service';
 
 import {
   RootComponent,
@@ -28,6 +37,12 @@ import {
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    StoreModule.forRoot({ profile: profileReducer }),
+    EffectsModule.forRoot([ProfileEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [
     ProfileService,
--- src/app/features/profile/components/profile/profile.component.ts
+++ src/app/features/profile/components/profile/profile.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { ProfileService } from '../../services/profile.service';
+import { Store, select, createSelector } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { LoadUser } from '../../services/profile.service';
+
+export const selectProfile = (state: any) => {
+  console.log({state});
+  return state.profile;
+};
+
+export const selectProfileData = createSelector(
+  selectProfile,
+  (state: any) => state.profile
+);
+
 
 @Component({
   selector: 'gp-profile',
@@ -9,13 +23,20 @@ import { ProfileService } from '../../services/profile.service';
 export class ProfileComponent implements OnInit {
   public profile: any;
   public ready = false;
+  profileNgrx$: Observable<any>;
 
   constructor (
     private profileService: ProfileService,
-  ) { }
+    private store: Store<any>
+  ) {
+    console.log('profile constructor');
+    this.store.select(selectProfile).subscribe(p => console.log(p));
+    this.profileNgrx$ = this.store.select(selectProfile);
+  }
 
   ngOnInit() {
     console.log('profile ngOnInit');
+    this.store.dispatch(new LoadUser());
     this.profileService.getProfile().subscribe((json) => {
       this.profile = json;
       this.ready = true;
